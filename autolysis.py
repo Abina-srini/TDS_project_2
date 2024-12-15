116abddb405ccc54726366c83b661cd1c8286ffd
Abina-srini <23f1001705@ds.study.iitm.ac.in>
2024-12-15 20:13:06 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,3 +1,17 @@
+# /// script
+requires-python = ">=3.11"
+dependencies = [
+  "httpx",
+  "pandas",
+  "matplotlib",
+  "seaborn",
+  "requests",
+  "ydata-profiling",
+  "scipy",
+  "numpy",
+  "textblob",
+]
+# ///
 import os
 import sys
 import pandas as pd
